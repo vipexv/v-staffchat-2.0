f4d6e56c8bbf01ed754ca79461786078ffbe948a
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-11 18:21:42 +0100

    perf: add million.js to improve performance

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,10 @@
+import million from 'million/compiler';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   base: "./",
   build: {
     outDir: "build",
