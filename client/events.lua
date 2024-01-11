0619002c163d00af5e25959a3bb5cf82c6c19359
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-11 19:04:49 +0100

    fix: throwing errors when using the next func if the param is null or not a table

--- client/events.lua
+++ client/events.lua
@@ -4,7 +4,7 @@ end)
 
 RegisterNetEvent("staffchat:client:admins", function(data)
   if not data then
-    return Debug("[staffchat:client:admins] Event was called but the first param is either null or not a table.")
+    return Debug("[staffchat:client:admins] Event was called but the first param null.")
   end
 
   Debug("[staffchat:client:admins] Data param:", json.encode(data))
@@ -13,8 +13,8 @@ RegisterNetEvent("staffchat:client:admins", function(data)
 end)
 
 RegisterNetEvent("staffchat:client:firemessage", function(data)
-  if not next(data) then
-    return Debug("[staffchat:client:firemessage] Event was called but data parm is null or not a table.")
+  if not data then
+    return Debug("[staffchat:client:firemessage] Event was called but data parm is null.")
   end
 
   Debug("[staffchat:client:firemessage] data param: ", json.encode(data))
@@ -22,8 +22,8 @@ RegisterNetEvent("staffchat:client:firemessage", function(data)
 end)
 
 RegisterNetEvent("staffchat:client:permissions", function(data)
-  if not next(data) then
-    return Debug("[staffchat:client:permissions] Event triggered, but the first param is either null or not a table.")
+  if not data then
+    return Debug("[staffchat:client:permissions] Event triggered, but the first param is null.")
   end
 
   UIMessage("staffchat:nui:sourcedata", data)
